403e0284bca8a3f640d80d9a8145b6771e8fdee9
RandyPrimaPutra <103095236+randyprimaputra@users.noreply.github.com>
2022-12-28 10:36:57 +0800

    update

--- flutter_apk/lib/db/sql_helper.dart
+++ flutter_apk/lib/db/sql_helper.dart
@@ -4,19 +4,26 @@ import 'package:sqflite/sqflite.dart' as sql;
 
 class SQLHelper {
   // fungsi membuat database
-  static Future<void> createTables(sql.Database database) async {
+static Future<void> createTables(sql.Database database) async {
+  // Check if the table already exists
+  final tables = await database.rawQuery('SELECT name FROM sqlite_master WHERE type = "table"');
+  final tableNames = tables.map((table) => table['name'] as String).toList();
+
+  // If the table does not exist, create it
+  if (!tableNames.contains('t_Membercard')) {
     await database.execute("""
-CREATE TABLE t_Membercard(
-  kode_member INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-  nama TEXT,
-  tanggal_lahir TEXT,
-  alamat TEXT,
-  jenis_kelamin TEXT,
-  username TEXT,
-  password TEXT,
-)
-""");
+      CREATE TABLE t_Membercard(
+        kode_member INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+        nama TEXT,
+        tanggal_lahir TEXT,
+        alamat TEXT,
+        jenis_kelamin TEXT,
+        username TEXT,
+        password TEXT
+      );
+    """);
   }
+}
 
   static Future<sql.Database> db() async {
     return sql.openDatabase('testjob.db', version: 1,
@@ -26,12 +33,34 @@ CREATE TABLE t_Membercard(
   }
 
   //tambah data
-  
-  static Future<int> tambahDatamember(String nama, String tanggal_lahir, String alamat, String jenis_kelamin, String username, String password ) async {
+
+static Future<int> tambahDatamember(
+    String nama,
+    String tanggal_lahir,
+    String alamat,
+    String jenis_kelamin,
+    String username,
+    String password) async {
+  try {
+    // Open the database and create the table if it does not exist
     final db = await SQLHelper.db();
-    final data = {'nama': nama,'tanggal_lahir': tanggal_lahir,'alamat': alamat,'jenis_kelamin': jenis_kelamin,'username': username, 'password': password};
+    await createTables(db);
+
+    // Insert the data into the table
+    final data = {
+      'nama': nama,
+      'tanggal_lahir': tanggal_lahir,
+      'alamat': alamat,
+      'jenis_kelamin': jenis_kelamin,
+      'username': username,
+      'password': password
+    };
     return await db.insert('t_Membercard', data);
+  } catch (e) {
+    print(e);
+    return 0;
   }
+}
 
   //ambil data
   static Future<List<Map<String, dynamic>>> getDatamember() async {
