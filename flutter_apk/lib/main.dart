31e41483c9cd733be9e455d140b6205a2fec9c6c
RandyPrimaPutra <103095236+randyprimaputra@users.noreply.github.com>
2022-12-28 21:20:22 +0800

    update
    pc

--- flutter_apk/lib/main.dart
+++ flutter_apk/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_apk/pages/admin_page.dart';
+import 'package:flutter_apk/pages/auth_page.dart';
 import 'package:flutter_apk/pages/login_page.dart';
 
 //import 'login_page.dart';
@@ -15,10 +16,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      // home: LoginPage(),
-      home: AdminPage(),
+      home: LoginPage(),
     );
   }
 }
--- flutter_apk/lib/pages/login_page.dart
+++ flutter_apk/lib/pages/login_page.dart
@@ -1,126 +1,133 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_apk/components/my_button.dart';
+import 'package:flutter_apk/components/my_textfield.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class LoginPage extends StatefulWidget {
-  const LoginPage({Key? key}) : super(key: key);
+  LoginPage({super.key});
 
   @override
   State<LoginPage> createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
+  // text editing controllers
+  final _usernameController = TextEditingController();
+
+  final _passwordController = TextEditingController();
+
+  // Sign user in method
+  void signUserIn() {}
+
+// wrong username message popup
+  void wrongUsernameMessage() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return const AlertDialog(
+          title: Text('Incorrect Username'),
+        );
+      },
+    );
+  }
+
+  // wrong password message popup
+  void wrongpasswordMessage() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return const AlertDialog(
+          title: Text('Incorrect Password'),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.green,
       body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Icon(
-                Icons.lock_person,
-                size: 100,
-                color: Colors.white,
-              ),
-              // Hello again !
-              Text(
-                'Hello Again !',
-                style: GoogleFonts.bebasNeue(
-                  fontSize: 36,
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 100),
+                const Icon(Icons.lock_person, size: 100, color: Colors.white),
+                // Hello again !
+                Text(
+                  'Login',
+                  style: GoogleFonts.bebasNeue(fontSize: 36),
                 ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              const Text(
-                'Welcome back, you\'ve been missed!',
-                style: TextStyle(
-                  fontSize: 24,
+                const SizedBox(height: 10),
+                const Text(
+                  'Required ',
+                  style: TextStyle(fontSize: 24),
                 ),
-              ),
-              const SizedBox(
-                height: 50,
-              ),
+                const SizedBox(height: 50),
 
-              // username textfield
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.green[200],
-                    border: Border.all(
-                      color: Colors.white,
-                    ),
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: const Padding(
-                    padding: EdgeInsets.only(left: 20.0),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        prefixIcon: Icon(Icons.person),
-                        hintText: 'Username',
-                      ),
-                    ),
-                  ),
+                // username textfield
+                // ignore: prefer_const_constructors
+                MyTextField(
+                  controller: _usernameController,
+                  hintText: 'Username',
+                  obsecureText: false,
                 ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
 
-              // password textfield
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.green[200],
-                    border: Border.all(
-                      color: Colors.white,
-                    ),
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: const Padding(
-                    padding: EdgeInsets.only(left: 20.0),
-                    child: TextField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        prefixIcon: Icon(Icons.key),
-                        hintText: 'Password',
-                      ),
-                    ),
-                  ),
+                const SizedBox(height: 10),
+                // password textfield
+                MyTextField(
+                  controller: _passwordController,
+                  hintText: 'Password',
+                  obsecureText: true,
                 ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
+                const SizedBox(height: 10),
 
-              // sign in button
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Container(
-                  padding: const EdgeInsets.all(20),
-                  decoration: BoxDecoration(
-                      color: Colors.green[700],
-                      borderRadius: BorderRadius.circular(12)),
-                  child: const Center(
-                    child: Center(
-                      child: Text(
-                        'Sign In',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18,
-                        ),
+                // forgot password
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: const [
+                      Text(
+                        'Forgot Password?',
                       ),
-                    ),
+                    ],
                   ),
                 ),
-              ),
-            ],
+
+                const SizedBox(height: 25),
+                // sign in button
+                MyButton(
+                  onTap: signUserIn,
+                ),
+                const SizedBox(height: 10),
+
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: const [
+                      Text(
+                        'Not have account? ',
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: const [
+                      Text(
+                        'Ask your Administrator for register',
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
